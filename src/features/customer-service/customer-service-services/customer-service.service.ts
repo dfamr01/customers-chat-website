ee7ae717b6ebf0f7036c15d2a14c8f61ba1ceed1
daivd_abu <david@livly.com>
2024-08-01 01:05:21 +0300

    test

--- src/features/customer-service/customer-service-services/customer-service.service.ts
+++ src/features/customer-service/customer-service-services/customer-service.service.ts
@@ -14,20 +14,33 @@ class CallService {
   public path = "/calls";
 
   constructor() {
-    // this.socket = io(ENV_VARS.VITE_API_SERVER, {
+    // this.socket = io(ENV_VARS.API_SERVER, {
     //   transports: ["websocket", "polling"],
     //   path: "/socket.io", // Make sure this matches the server-side path
     // });
+
+    // const socketURL = process.env.NODE_ENV === 'production'
+    // ? 'wss://customers-support-bh7no.ondigitalocean.app'
+    // : 'ws://localhost:3000'; // Assuming your backend runs on port 3000 locally
 
     this.socket = io(ENV_VARS.VITE_WEBSOCKET_SERVER, {
       transports: ["websocket"],
       withCredentials: true,
       forceNew: true,
-      timeout: 10000, // Increase timeout
+      timeout: 10000,
       path: "/customers-chat-server2/socket.io",
     });
 
-    // this.socket = io(ENV_VARS.VITE_API_SERVER);
+    // this.socket = io(ENV_VARS.WEBSOCKET_SERVER, {
+    //   transports: ["websocket"],
+    //   // transports: ["websocket", "polling"],
+    //   withCredentials: true,
+    //   forceNew: true,
+    //   timeout: 10000, // Increase timeout
+    //   path: "/socket.io", // Make sure this matches the server-side path
+    // });
+
+    // this.socket = io(ENV_VARS.API_SERVER);
     // Check for successful connection
     this.socket.on("connect", () => {
       console.info("Connected to the websocket server");
@@ -41,7 +54,7 @@ class CallService {
 
   async getAddresses(query: string): Promise<Address[]> {
     const response = await axios.get(
-      `${ENV_VARS.VITE_API_SERVER}${this.path}/addresses`,
+      `${ENV_VARS.API_SERVER}${this.path}/addresses`,
       {
         params: { q: query },
       }
@@ -50,7 +63,7 @@ class CallService {
   }
 
   async getAllCalls(): Promise<Record<string, Call>> {
-    const response = await axios.get(`${ENV_VARS.VITE_API_SERVER}${this.path}`);
+    const response = await axios.get(`${ENV_VARS.API_SERVER}${this.path}`);
     return response.data.data;
   }
 
