054000cc0feb9cec22873548d9e76f480f510d1a
daivd_abu <david@livly.com>
2024-09-12 13:28:40 +0300

    done

--- src/features/home/home-components/HomeContainer/index.tsx
+++ src/features/home/home-components/HomeContainer/index.tsx
@@ -2,14 +2,16 @@ import { Box, Button, Paper, styled } from "@mui/material";
 import { ScreensRoutes } from "../../../../shared/router/routes";
 import { Link as RouterLink } from "react-router-dom";
 import logo from "../../../../../public/android-chrome-512x512.png";
+import { userService } from "../../../users/users-services/users.service";
+import { useEffect, useState } from "react";
 // import logo from "../../../../../public/android-chrome-192x192.png";
-interface AppProps {}
+interface AppProps { }
 
 interface ButtonOwnProps {
   to: string;
 }
 
-const ButtonAgent = styled(Button)<ButtonOwnProps>`
+const ButtonAgent = styled(Button) <ButtonOwnProps>`
   /* background-color: #bb4ce22e; */
   margin-bottom: 10px;
 `;
@@ -20,7 +22,7 @@ const Logo = styled("img")`
   width: 200px;
 `;
 
-const ButtonClient = styled(Button)<{ to: string }>`
+const ButtonClient = styled(Button) <{ to: string }>`
   /* background-color: #e24c4c2e; */
 `;
 
@@ -31,34 +33,96 @@ export const PaperWrapper = styled(Paper)`
   padding: 15px;
 `;
 
-const HomeContainer: React.FC<AppProps> = () => {
+const mockUpdatePolicy = {
+  "rules": [
+    {
+      "id": 1,
+      "type": "exists",
+      "displayName": "A Exists 1111",
+      "priority": 1,
+      "condition": "A"
+    },
+    {
+      "id": 2,
+      "type": "exists",
+      "displayName": "B Exists 22222",
+      "priority": 2,
+      "condition": "B"
+    },
+    {
+      "id": 3,
+      "type": "exists",
+      "displayName": "C Exists",
+      "priority": 3,
+      "condition": "C"
+    },
+    {
+      "id": 4,
+      "type": "exists",
+      "displayName": "D Exists",
+      "priority": 4,
+      "condition": "D"
+    }
+  ]
+}
+
+const RULES = "ABC"
+const HomeContainer: React.FC<AppProps> = ({ periodCheckMS = 1000, rules = RULES }) => {
+  const [policyData, setDataPolicy] = useState({});
+  // const [policyHash, setPolicyHash] = useState("");
+
+  useEffect(() => {
+    const fetchPolicy = async () => {
+      try {
+        const policy = await userService.getPolicy()
+        setDataPolicy(policy)
+      } catch (err) {
+        console.error("🚀 ~ fetchPolicy ~ err:", err)
+
+      }
+    }
+
+    const checkHasPolicyModified = async () => {
+      try {
+        const hash = await userService.getPolicyHash();
+        setDataPolicy((current) => {
+          if (current.hash != hash) {
+            fetchPolicy();
+          }
+          return current;
+        })
+      } catch (err) {
+        console.log("🚀 ~ checkHasPolicyModified ~ err:", err)
+
+      }
+
+
+    }
+    fetchPolicy();
+    let intervalId = setInterval(checkHasPolicyModified, periodCheckMS)
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId)
+        intervalId = null;
+      }
+    }
+  }, [])
+
+  const updatePolicy = () => {
+    userService.updatePolicy(mockUpdatePolicy)
+  }
+
   return (
     <PaperWrapper>
-      <Box>
-        <Logo
-          // srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-          src={logo}
-          alt="logo"
-          loading="lazy"
-        />
-      </Box>
-      <ButtonAgent
-        // @ts-expect-error - Property 'to' does not exist on type 'IntrinsicAttributes & ButtonProps & { children?: ReactNode; }'.
-        component={RouterLink}
-        to={ScreensRoutes.Login}
-        variant="contained"
-        color="secondary"
-      >
-        Customer
-      </ButtonAgent>
-      <ButtonClient
-        // @ts-expect-error - Property 'to' does not exist on type 'IntrinsicAttributes & ButtonProps & { children?: ReactNode; }'.
-        component={RouterLink}
-        to={ScreensRoutes.CustomerService}
-        variant="contained"
-      >
-        Chat Representative
-      </ButtonClient>
+
+      <button onClick={updatePolicy}>
+        update
+      </button>
+      <div>
+        {
+          JSON.stringify(policyData.policy)
+        }
+      </div>
     </PaperWrapper>
   );
 };
--- src/features/users/users-services/users.service.ts
+++ src/features/users/users-services/users.service.ts
@@ -5,15 +5,39 @@ import { Address } from "../users-types/users.types";
 class UsersService {
   public path = "/users";
 
-  async getAddresses(query: string): Promise<Address[]> {
+  // async getAddresses(query: string): Promise<Address[]> {
+  //   const response = await axios.get(
+  //     `${ENV_VARS.VITE_API_SERVER}${this.path}/addresses`,
+  //     {
+  //       params: { q: query },
+  //     }
+  //   );
+  //   return response.data.data;
+  // }
+
+  async getPolicy() {
+    const response = await axios.get(`${ENV_VARS.VITE_API_SERVER}${this.path}`);
+    return response.data.data;
+  }
+
+  async getPolicyHash(): Promise<string> {
     const response = await axios.get(
-      `${ENV_VARS.VITE_API_SERVER}${this.path}/addresses`,
-      {
-        params: { q: query },
-      }
+      `${ENV_VARS.VITE_API_SERVER}${this.path}/getPolicyHash`
     );
     return response.data.data;
   }
+
+  async updatePolicy(newPolicy) {
+    const response = await axios.post(
+      `${ENV_VARS.VITE_API_SERVER}${this.path}/admin`,
+      {
+        data: newPolicy,
+      }
+    );
+    console.log("🚀 ~ UsersService ~ updatePolicy ~ response:", response);
+    return newPolicy;
+  }
 }
 
-export { UsersService };
+const userService = new UsersService();
+export { UsersService, userService };
